bf0ec02191c6e13d3db87aadd2f27ffc8f654013
VVC255 <VVC255@MUS251936.cof.ds.capitalone.com>
2017-01-11 17:27:43 -0500

    handle precedence

--- src/mylib/calculator/impl/IntCalculator.java
+++ src/mylib/calculator/impl/IntCalculator.java
@@ -6,82 +6,81 @@ import java.util.LinkedList;
 import mylib.calculator.exception.IllegalOperatorException;
 import mylib.calculator.spec.Calculator;
 
-public class IntCalculator implements Calculator<Integer>{
+public class IntCalculator implements Calculator<Integer> {
 
-	@Override
-	public Integer add(Integer operand1, Integer operand2) {	
-		return operand1 + operand2;
-	}
+    @Override
+    public Integer add(Integer operand1, Integer operand2) {
+        return operand1 + operand2;
+    }
 
-	@Override
-	public Integer sub(Integer operand1, Integer operand2) {
-		return operand1 - operand2;
-	}
+    @Override
+    public Integer sub(Integer operand1, Integer operand2) {
+        return operand1 - operand2;
+    }
 
-	@Override
-	public Integer mul(Integer operand1, Integer operand2) {
-		return operand1 * operand2;
-	}
+    @Override
+    public Integer mul(Integer operand1, Integer operand2) {
+        return operand1 * operand2;
+    }
 
-	@Override
-	public Integer div(Integer operand1, Integer operand2) {
-		return operand1 / operand2;
-	}
+    @Override
+    public Integer div(Integer operand1, Integer operand2) {
+        return operand1 / operand2;
+    }
 
-	@Override
-	public Integer eval(String expression){
-		// create stacks for operators and operands
-		Deque<Character> op = new LinkedList<>();
-		Deque<Integer> val = new LinkedList<>();
+    @Override
+    public Integer eval(String expression) {
 
-		//insert a '0' at front to balance expression
-		String input = "0" + expression.replaceAll("-", "+-");
-		StringBuilder temp = new StringBuilder();
+        Deque<Character> operatorStack = new LinkedList<>();
+        Deque<Integer> operandStack = new LinkedList<>();
 
-		//tokenize the expression into numbers an operands
-		for (int i = 0; i < input.length(); i++){
-			char ch = input.charAt(i);
-			if(ch == '-'){
-				temp = temp.insert(0,  '-');
-			}
-			else if(ch!= '+' && ch != '*' && ch != '/'){
-				temp.append(ch);
-			}
-			else{
-				if(temp.length() != 0){
-					val.push(Integer.parseInt(temp.toString()));
-				}
-				op.push(ch);
-				temp.setLength(0);
-			}
+        StringBuilder operand = new StringBuilder();
+        String input = "0" + expression.replaceAll("-", "+-");
 
-		}
-		val.push(Integer.parseInt(temp.toString()));
+        for (int i = 0; i < input.length(); i++) {
+            char token = input.charAt(i);
+            if (token != '+' && token != '*' && token != '/') {
+                operand.append(token);
+            } else {
+                if (operand.length() != 0) {
+                    operandStack.push(Integer.parseInt(operand.toString()));
+                    operand.setLength(0);
+                }
+                Character operator = operatorStack.peek();
+                while (operator != null && ('*' == operator || '/' == operator)) {
+                    int operand2 = operandStack.pop();
+                    int operand1 = operandStack.pop();
+                    operandStack.push(calculate(operand1, operand2, operatorStack.pop()));
+                    operator = operatorStack.peekLast();
+                }
+                operatorStack.push(token);
+            }
+        }
+        operandStack.push(Integer.parseInt(operand.toString()));
+        while (operatorStack.peek() != null) {
+            int operand2 = operandStack.pop();
+            int operand1 = operandStack.pop();
+            char operator = operatorStack.pop();
+            operandStack.push(calculate(operand1, operand2, operator));
+        }
+        return operandStack.pop();
+    }
 
-		//calculate the expression
-		while(!op.isEmpty()){
-			char operator = op.pop();
-			int operand2 = val.pop();
-			int operand1 = val.pop();
-
-			switch(operator){
-				case '+' :
-					val.push(add(operand1, operand2));
-					break;
-	
-				case '*' :
-					val.push(mul(operand1, operand2));
-					break;
-	
-				case '/' :
-					val.push(div(operand1, operand2));
-					break;
-	
-				default:
-					throw new IllegalOperatorException("not a valid operator");
-				}
-		}
-		return val.pop();
-	}
-	
+    private Integer calculate(Integer operand1, Integer operand2, Character operator) {
+        Integer result;
+        switch (operator) {
+            case '+':
+                result = add(operand1, operand2);
+                break;
+            case '*':
+                result = mul(operand1, operand2);
+                break;
+            case '/':
+                result = div(operand1, operand2);
+                break;
+            default:
+                throw new IllegalOperatorException("not a valid operator");
+        }
+        return result;
+    }
 }
